8e5c83f994bc49965e71dcec9c2b624f5110750b
Michal <majkeltybi@gmail.com>
2023-07-13 21:51:58 +0200

    Added Main page and Search page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
+import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import {HomePage} from "./layouts/HomePage/HomePage";
+import {SearchBooksPage} from "./layouts/SearchBooksPage/SearchBooksPage";
+import {Redirect, Route, Switch} from "react-router-dom";
+import {BookCheckoutPage} from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className='d-flex flex-column min-vh-100'>
+            <Navbar/>
+            <div className='flex-grow-1'>
+                <Switch>
+                    <Route path='/' exact>
+                        <Redirect to='/home'/>
+                    </Route>
+                    <Route path='/home'>
+                        <HomePage/>
+                    </Route>
+                    <Route path='/search'>
+                        <SearchBooksPage/>
+                    </Route>
+                    <Route path='/checkout/:bookId'>
+                        <BookCheckoutPage/>
+                    </Route>
+                </Switch>
+            </div>
+            <Footer/>
+        </div>
+    );
 }
-
-export default App;
